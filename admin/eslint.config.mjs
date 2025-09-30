9f61741f25f7fcd62bca2ccc588250752b096274
Awongo Fahadi Rashid <fahadirashidawongo@gmail.com>
2025-10-01 00:14:11 +0300

    fixed all errors

--- admin/eslint.config.mjs
+++ admin/eslint.config.mjs
@@ -1,7 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import baseConfig from '../eslint.config.base.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,7 +11,24 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  baseConfig,
+  {
+    languageOptions: {
+      parser: (await import('@typescript-eslint/parser')).default,
+    },
+    plugins: {
+      '@typescript-eslint': (await import('@typescript-eslint/eslint-plugin')).default,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-empty-object-type': 'warn',
+      'prefer-const': 'warn',
+      'no-var': 'warn',
+      'react/no-unescaped-entities': 'warn',
+      'react-hooks/exhaustive-deps': 'warn',
+      '@next/next/no-img-element': 'warn',
+    },
+  },
   {
     ignores: [
       "node_modules/**",

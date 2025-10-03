2fff77a3f6b7166865feb2b8853098cd048f4bbd
Awongo Fahadi Rashid <fahadirashidawongo@gmail.com>
2025-10-03 21:28:13 +0300

    removed user-site

--- user-site/eslint.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('eslint').Linter.Config} */
-const config = {
-  extends: ['../eslint.config.base.js'],
-  rules: {
-    // Add any user-site specific rules here
-  },
-};
-
-module.exports = config;
